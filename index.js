d3e9ba3bbe0e289989e97186bbc2c43c5a4e951a
Jay <jbarbanel531@gmail.com>
2021-12-14 17:42:56 -0800

    added functions for intern and egineer to the Index.js file

--- index.js
+++ index.js
@@ -29,10 +29,67 @@ function addManager() {
     })
 }
 
-function nextPrompt() {
-    inquirer.prompt([
+function addEngineer() {
+    inquirer.prompt([{
+        type: 'input',
+        message: 'What is the Engineers name?',
+        name: 'name',
+    }, {
+        type: 'input',
+        message: 'What is the Engineers email?',
+        name: 'email',
+    }, {
+        type: 'input',
+        message: 'What is the Engineers id?',
+        name: 'id',
+    }, {
+        type: 'input',
+        message: 'What is the Engineers Github?',
+        name: 'gitHub',
+    }]).then(function(data) {
+        console.log(data)
+        const engineers = new Engineer(data.name, data.email, data.id, data.officeNumber)
+        employees.push(engineer)
+        nextPrompt()
+    })
+}
 
-    ])
+function addIntern() {
+    inquirer.prompt([{
+        type: 'input',
+        message: 'What is the Interns name?',
+        name: 'name',
+    }, {
+        type: 'input',
+        message: 'What is the Interns email?',
+        name: 'email',
+    }, {
+        type: 'input',
+        message: 'What is the Interns id?',
+        name: 'id',
+    }, {
+        type: 'input',
+        message: 'What school did the intern go to?',
+        name: 'officeNumber',
+    }]).then(function(data) {
+        console.log(data)
+        const intern = new Intern(data.name, data.email, data.id, data.officeNumber)
+        employees.push(intern)
+        nextPrompt()
+    })
+}
+
+function nextPrompt() {
+    inquirer.prompt([{
+        type: 'list',
+        message: 'What do you want to add next?',
+        name: 'option',
+        choices: ['Engineer', 'Intern', 'Quit']
+    }]).then(function(data) {
+        if (data.option == "Engineer") {
+
+        }
+    })
 }
 
 addManager()
